70f83d0187bd80b0dd75c132a4ac2c210885152f
Axel Verrier <axel.verrier7@gmail.com>
2026-03-16 10:59:06 +0100

    Commit 2 - updated with theatres nationaux data

--- data.js
+++ data.js
@@ -3,7 +3,9 @@ const TYPE_COLORS = {
   "Classique":        "#4a7fc1",
   "Contemporain":     "#c05050",
   "Comédie":          "#d49010",
+  "Drame":            "#8b3a3a",
   "Danse":            "#7c5cb5",
   "Opéra":            "#2a9d8f",
+  "Opérette":         "#3a9d8f",
   "Comédie musicale": "#c45490",
 };
